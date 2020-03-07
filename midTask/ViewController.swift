e4492ef35034c9c78391eb188087c0fa7eb85802
assemsemsem <assemsarsenbay08@gmail.com>
2020-03-07 17:08:58 +0600

    Initial Commit

---
+++ midTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  midTask
+//
+//  Created by Assem on 3/7/20.
+//  Copyright © 2020 Assem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
